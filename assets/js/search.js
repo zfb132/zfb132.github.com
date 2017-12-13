e5afacac1076941b5f5e403d754a37496e26b302
Fubin Zhang <zfb132@gmail.com>
2017-12-13 16:40:08 +0800

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -64,7 +64,7 @@
         'content': window.store[key].content
       });
 
-      var results = idx.search(searchTerm); // Get lunr to perform a search
+      var results = this.search(searchTerm); // Get lunr to perform a search
       displaySearchResults(results, window.store, searchTerm);
     }
     });
